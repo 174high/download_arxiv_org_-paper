95a4c10ab11d1e1bf94dc34c8ab89e3ba6c74805
Johnny SHI <johnny.shi@schindler.com>
2019-10-29 16:17:11 +0800

    can download titile addr number

--- test_chrome.py
+++ test_chrome.py
@@ -50,6 +50,8 @@ class crawler():
         print("loading: %s" % kwargs.get('request').get('url'))
 
     def call_web(self): 
+        list = []
+        list2= []
 
         # call method with timeout
         self.tab.call_method('Page.navigate',url="https://arxiv.org/search/?query="+"Autonomous+Vehicle"+"&searchtype=all&source=header&order=-announced_date_first&size=50&abstracts=show&start=0", _timeout=5)
@@ -62,21 +64,22 @@ class crawler():
         #print(html)
         soup = BeautifulSoup(((html['result'])['value']),"html.parser")
 
-        number_pdf=0
-        control_1=0
+
 
         for link in soup.find_all("p"):
 #            print(link)
 #            print(type(str(link)))   
             if str(link).find("title is-5 mathjax") > 0 :
                 print(link) 
-                a=str(link).find("<p class="title is-5 mathjax">")
+                a=str(link).find('''<p class="title is-5 mathjax">''')
                 b=str(link).find("</p>")
-                title=str(link)[a+30:b-5]
-		
+                title=str(link)[a+46:b-4]
+                list.append(title)
 
-#                self.excel("./tamplate/","arxiv.xlsx","./files/","arxiv-total.xlsx",None,title,None,None,None,None,None)
+        for i in list:
+            print(i)
 
+        number_pdf=0
 
         for link in soup.find_all("a"):
 #            print(link)
@@ -91,11 +94,26 @@ class crawler():
                 pdf_name=str(link)[a+4:a+14]
                 print(pdf_name)
                 number_pdf=number_pdf+1
-                self.excel("./tamplate/","arxiv.xlsx","./files/","arxiv-total.xlsx",pdf_name,None,None,addr,None,None,None)
+                D1={}
+                D1["serial_num"]=pdf_name
+                D1["addr"]=addr	
+                print(D1)
+                list2.append(D1)
 
-               
 
-    def excel(self,path,name,output,name2,serial_num,titile,abstract,addr,existence,date,position):
+#        print("how mamy =")
+#        print(number_pdf)
+#        print(len(list2))
+#        print(len(list))
+
+
+        for i in list2:
+            i["title"]=list.pop(0)  
+            print(i['serial_num'],i['addr'],i["title"])   
+            self.excel("./tamplate/","arxiv.xlsx","./files/","arxiv-total.xlsx",i['serial_num'],i["title"],i['addr'],None,None,None)
+                
+
+    def excel(self,path,name,output,name2,serial_num,titile,addr,existence,date,position):
     
         print(path)
         print(output)
@@ -120,6 +138,7 @@ class crawler():
                 exist=False
                 while(file_num<=sheet.max_row):
                     if(sheet.cell(row=file_num, column=1).value==serial_num):
+                        print("is true!")
                         exist=True
                         break
                     file_num=file_num+1 
@@ -130,17 +149,15 @@ class crawler():
                     if(serial_num!=None):
                         sheet.cell(row=next_row, column=1,value=serial_num)
                     if(titile!=None):
-                    sheet.cell(row=next_row, column=2,value=titile)
-                    if(abstract!=None):
-                    sheet.cell(row=next_row, column=3,value=abstract)
+                        sheet.cell(row=next_row, column=2,value=titile)
                     if(addr!=None):
-                    sheet.cell(row=next_row, column=4,value=addr)
+                        sheet.cell(row=next_row, column=3,value=addr)
                     if(existence!=None):
-                    sheet.cell(row=next_row, column=5,value=existence)
+                        sheet.cell(row=next_row, column=4,value=existence)
                     if(date!=None):
-                    sheet.cell(row=next_row, column=6,value=date)
+                        sheet.cell(row=next_row, column=5,value=date)
                     if(position!=None):
-                    sheet.cell(row=next_row, column=7,value=position)
+                        sheet.cell(row=next_row, column=6,value=position)
 
                 wb.save(output+"tmp.xlsx")
                                 
